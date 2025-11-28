b2609188a04f8944a272feee6cd8ff963609bd4c
veeranjaneyuluchavvakula <veeranjaneyuluchavvakula1@gmail.com>
2025-11-28 16:05:57 +0530

    second

---
+++ day-2/variables.tf
@@ -0,0 +1,16 @@
+variable "ami" {
+  type = string
+  default = "ami-0a9098891d675c629"
+}
+
+variable "ins" {
+    type = string
+    default = "t2.micro"
+  
+}
+
+variable "ke" {
+    type = string
+    default = "veera"
+  
+}
