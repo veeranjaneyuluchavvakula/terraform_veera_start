357d45b27538cc4c0e52c24079349bb81fd83ac8
veeranjaneyuluchavvakula <veeranjaneyuluchavvakula1@gmail.com>
2025-12-01 11:25:32 +0530

    day-3

--- day-3/main.tf
+++ day-3/main.tf
@@ -1,4 +1,10 @@
 resource "aws_instance" "name" {
-    ami = ""
+    ami = "ami-0a9098891d675c629"
+    instance_type = "t2.micro"
+    key_name = "veera"
+    tags = {
+      Name = "dev"
+    
+    }
   
 }
